b8e6e3aba133311d24f2445732fe5b3811861d9f
Vincentguo <Vincentguo@anjuke.com>
2013-08-31 23:54:17 +0800

    md

--- view/default/custom.php
+++ view/default/custom.php
@@ -2,7 +2,7 @@
     <div class="row-fluid"  id="msg">
 
     </div>
-    <div class="row-fluid hero-unit" style="text-align: center;">
+    <div class="row-fluid" style="text-align: center;">
         <form class="form-inline">
         编码:<input type="text" id="code" placeholder="请输入发射编码"/>
             <input type="button" class="btn" value="发送" id="command"/>
